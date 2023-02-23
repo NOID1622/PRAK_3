5636724b56494b27d50bc7c916785431362d6b05
NOID1622 <112671264+NOID1622@users.noreply.github.com>
2023-02-23 17:43:07 +0700

    afasdf

--- nilaiukur.py
+++ nilaiukur.py
@@ -30,3 +30,29 @@ elif nilai>=nilai_d:
     print('D')
 else :
     print('E')
+    
+    
+#auiwgqr
+#input
+player1 = int(input("masukan nilai player 1 : "))
+player2 = int(input("masukan nilai player 2 : "))
+player3 = int(input("masukan nilai player 3 : "))
+
+#proses and output
+if player1 == player2 == player3:
+    print("tidak ada yang pemenang ")
+elif player1 > 21 and player2 >21 and player3 > 21:
+    print("tidak ada yang pemenang")
+elif player1 <= 21 and player1 > player2 and player1 > player2:
+    print("pemenangnya adalah p1")
+elif player2 <= 21 and player2 > player1 and player2 > player3: 
+    print("pemenangnya p2")
+elif player1 > 21 and player2 <= 21 and player3 > 21:
+    print("pemenangnya p2")
+elif player3 <= 21 and player3 > player1 and player3 > player2:
+    print("pemenangnya pemain 3")
+elif player1 <= 21 and abs(21 - player2) and abs(21-player1) and abs (21 - player3):
+    print("maka tidak ada pemenang")
+else:
+    print('input anda salah')
+
